7690078b4cb2881d49f96a052935f4898c37d9b1
adriel419 <adrielkotorri@nmc.al>
2023-08-30 14:51:23 +0200

    fixed clerk issue with publishable key

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // to protect all routes
 export default authMiddleware({
   publicRoutes: ["/api/:path*"],
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
 
 export const config = {
