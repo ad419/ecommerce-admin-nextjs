95fa9ca848f9a9727878ad06e46adb98723e8c21
adriel419 <adrielkotorri@nmc.al>
2023-09-19 14:55:29 +0200

    customer-atuh

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/api/:path*"],
   secretKey: process.env.CLERK_SECRET_KEY,
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/"],
 });
 
 export const config = {
