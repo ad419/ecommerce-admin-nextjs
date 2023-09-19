9aaecadd1a319367c5422c23a275a2f841ec24be
adriel419 <adrielkotorri@nmc.al>
2023-09-19 15:07:16 +0200

    fixed clerk

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // to protect all routes
 export default authMiddleware({
-  publicRoutes: ["/api/:path*", "/"],
+  publicRoutes: ["/", "/api/:path*"],
   secretKey: process.env.CLERK_SECRET_KEY,
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/"],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
