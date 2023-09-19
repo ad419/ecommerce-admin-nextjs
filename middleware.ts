1cafdd2e8760274c0f7562e8173899b94fb05a59
adriel <64656327+ad419@users.noreply.github.com>
2023-09-19 15:19:22 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // to protect all routes
 export default authMiddleware({
   publicRoutes: [
-    "/",
+    "/*",
     "/sign-in",
     "/88e3a88e-561f-473d-b88a-2780c6fc6a76/billboards",
     "/88e3a88e-561f-473d-b88a-2780c6fc6a76/billboards/new",
@@ -12,7 +12,6 @@ export default authMiddleware({
   secretKey: process.env.CLERK_SECRET_KEY,
   ignoredRoutes: ["/api/webhook/clerk"],
 });
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
